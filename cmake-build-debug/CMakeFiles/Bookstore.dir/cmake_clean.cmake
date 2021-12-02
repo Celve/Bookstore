5c7c1b05d0b11016f9fa7e2fbaf7407548aad50c
Okami <limerancy@gmail.com>
2021-12-02 21:17:30 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Bookstore.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Bookstore"
+  "Bookstore.pdb"
+  "CMakeFiles/Bookstore.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bookstore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
